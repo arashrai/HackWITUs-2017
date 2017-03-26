6131fc3e03ff6fc2ed4499b64f692814582695fb
arashrai <arashrai17@gmail.com>
2017-03-26 02:51:26 -0400

    api.py returns body data in post

--- api.py
+++ api.py
@@ -13,11 +13,12 @@ api = Api(app)
 
 class hack(Resource):
     def get(self):
-        return {'get_success': 'true', 'data': request.data}
+        return {'get_success': 'true'}
 
     def post(self):
+        request.get_data()
         # subprocess.call('adb shell input tap', shell=True)
-        return {'post_success': 'true', 'data': request.data}
+        return {'post_success': 'true', 'data': request.json}
 
 
 api.add_resource(hack, '/hack')
