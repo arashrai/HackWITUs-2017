c81b8b6c9364dc7ea15931d8981bce100f93a6c7
Arash Rai <arashrai@Arashs-MacBook-Pro.local>
2017-03-26 06:23:23 -0400

    api done

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
+from flask import send_file
 import subprocess
+import os
 import requests
 
 # Python 3
@@ -12,13 +14,19 @@ api = Api(app)
 
 
 class hack(Resource):
+    def get_img(self):
+        os.system('adb shell screencap {}'.format('/sdcard/screen.png'))
+        os.system('adb pull {} {}'.format('/sdcard/screen.png', 'screen.png'))
+        return send_file('screen.png', mimetype='image/gif')
+
     def get(self):
-        return {'get_success': 'true'}
+        return self.get_img()
 
     def post(self):
         request.get_data()
-        # subprocess.call('adb shell input tap', shell=True)
-        return {'post_success': 'true', 'data': request.json}
+        x = int(request.json['x'])
+        y = int(request.json['y'])
+        os.system('adb shell input tap {} {}'.format(x, y))
 
 
 api.add_resource(hack, '/hack')
